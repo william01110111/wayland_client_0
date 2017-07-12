c5c308cb7f6647ec932bcf9e03bfeda73cf577ba
William W Wold <wilcoze@gmail.com>
2017-07-11 18:36:10 -0700

    copied in code to get the registry

--- main.cpp
+++ main.cpp
@@ -1,19 +1,74 @@
-
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <wayland-client.h>
 
 struct wl_display *display = NULL;
+struct wl_compositor *compositor = NULL;
 
-int main(int argc, char **argv) {
+void global_registry_handler
+	(
+		void *data,
+		struct wl_registry *registry,
+		uint32_t id,
+		const char *interface,
+		uint32_t version
+	)
+{
+	printf("Got a registry event for %s id %d\n", interface, id);
+	
+	if (strcmp(interface, "wl_compositor") == 0)
+	{
+		compositor = (wl_compositor*)wl_registry_bind
+			(
+				registry, 
+				id, 
+				&wl_compositor_interface, 
+				1
+			);
+	}
+}
 
-	display = wl_display_connect(NULL);
-	if (display == NULL) {
+void global_registry_remover(void *data, struct wl_registry *registry, uint32_t id)
+{
+	printf("Got a registry losing event for %d\n", id);
+}
+
+const wl_registry_listener registry_listener =
+	{
+		global_registry_handler,
+		global_registry_remover
+	};
+
+
+int main(int argc, char **argv)
+{
+	display = wl_display_connect(nullptr);
+	
+	if (display == NULL)
+	{
 		fprintf(stderr, "Can't connect to display\n");
 		exit(1);
 	}
-	printf("connected to display\n");
 	
+	printf("connected to display\n");
+
+	struct wl_registry *registry = wl_display_get_registry(display);
+	wl_registry_add_listener(registry, &registry_listener, NULL);
+
+	wl_display_dispatch(display);
+	wl_display_roundtrip(display);
+
+	if (compositor == NULL)
+	{
+		fprintf(stderr, "Can't find compositor\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Found compositor\n");
+	}
+
 	wl_display_disconnect(display);
 	printf("disconnected from display\n");
 

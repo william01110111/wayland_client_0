adcb271b0af9ac0c9c72b2f3867ff4dfae7eece5
William W Wold <wilcoze@gmail.com>
2017-09-15 00:35:32 -0700

    added wl_registry_listener

--- main.c
+++ main.c
@@ -34,13 +34,6 @@ void global_registry_remover(void *data, struct wl_registry *registry, uint32_t 
 	printf("Got a registry losing event for %d\n", id);
 }
 
-const struct wl_registry_listener registry_listener =
-	{
-		global_registry_handler,
-		global_registry_remover
-	};
-
-
 int main(int argc, char **argv)
 {
 	display = wl_display_connect(NULL);
@@ -54,6 +47,13 @@ int main(int argc, char **argv)
 	printf("connected to display\n");
 	
 	struct wl_registry *registry = wl_display_get_registry(display);
+	
+	const struct wl_registry_listener registry_listener =
+		{
+			global_registry_handler,
+			global_registry_remover
+		};
+	
 	wl_registry_add_listener(registry, &registry_listener, NULL);
 	
 	wl_display_dispatch(display);
